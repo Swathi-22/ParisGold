ea13d885f1f8b14d13a5bb1a2d208a02a0286ed4
swathi-22 <mkswathisuresh@gmail.com>
2022-03-31 16:51:45 +0530

    ContactDetails

--- web/forms.py
+++ web/forms.py
@@ -9,8 +9,8 @@ class ContactForm(forms.ModelForm):
         fields='__all__'
         widgets={
             'name':TextInput(attrs={'placeholder':"Your full name",'class':"form-input"}),
-            'phone':TextInput(attrs={'placeholder':"Enter email address",'class':"form-input"}),
-            'email':EmailInput(attrs={'placeholder':"Add phone number",'class':"form-input"}),
+            'phone':TextInput(attrs={'placeholder':"Add phone number",'class':"form-input"}),
+            'email':EmailInput(attrs={'placeholder':"Enter email address",'class':"form-input"}),
             'subject':TextInput(attrs={'placeholder':"Select Subject",'class':"form-input"}),
             'message':Textarea(attrs={'class':'form-control','placeholder':"Enter messages",'class':"form-textarea"}),
          }
